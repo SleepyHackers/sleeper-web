aca968a8a83530b5966e6ee22aee42829fd35936
Amber Sage <amber@gnu.org>
2015-11-07 18:01:30 -0500

    bigger chat box

--- js/demo_06.js
+++ js/demo_06.js
@@ -40,15 +40,15 @@ addEventListener("load", function() {
 	//label.hide_when = function () { return demo.loading_screen.is_active; };
 	label.innerHTML = "" +
 	    //'<div style="height: 80px; border-bottom: 1px dashed rgb(0, 0, 0); margin-bottom: 5px;">Message box</div>' +
-	    '<div id="fillbox" style="height:80px; overflow:hidden; margin-bottom:4px;"></div><div id="chatbox" style="border-top: 1px dashed rgb(0, 0, 0); padding-top: 5px;"><div>$ <span id="chat_input"></span></div></div>';
+	    '<div id="fillbox" style="height:180px; overflow:hidden; margin-bottom:4px;"></div><div id="chatbox" style="border-top: 1px dashed rgb(0, 0, 0); padding-top: 5px;"><div>$ <span id="chat_input"></span></div></div>';
 	label.style.backgroundColor = "rgba(255, 255, 255, 0.4)";
 	label.style.fontSize = "12px";
 	label.style.padding = "4px";
 	label.style.borderRadius = "4px";
 	label.style.left = "10px";
 	label.style.bottom = "10px";
-	label.style.height = "100px";
-	label.style.width = "230px";
+	label.style.height = "200px";
+	label.style.width = "330px";
 	//label.style.pointerEvents = "auto"; // restore mouse events
 
     // setup asset search paths
